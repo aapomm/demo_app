310a366cc7508246bad04c3b82ed714134187836
Aaron Manaloto <aaronpomanaloto@gmail.com>
2016-08-20 21:00:58 +0800

    Add found implementation

--- app/controllers/persons_controller.rb
+++ app/controllers/persons_controller.rb
@@ -2,11 +2,19 @@ class PersonsController <  ApplicationController
 
 
   def index
-    @persons = Person.all
+    @persons = Person.missing
   end
 
   def search
     @persons = Person.fulltext_search(params[:query]).records
+  end
+
+  def set_as_found
+    @person = Person.find params[:id]
+
+    MissingPersonsHandler.new.mark_person_as_found @person
+
+    redirect_to :persons
   end
 
 end
--- app/models/person.rb
+++ app/models/person.rb
@@ -2,4 +2,7 @@ class Person < ActiveRecord::Base
 
   include Searchable
 
+
+  scope :missing, -> { where :found => false }
+
 end
