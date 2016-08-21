90c12cbdf16a3aeea45ba02bd9a3eeedb3864b12
Ian De La Cruz <IanDeLaCruz@RIanDeLaCruz-MacBook-Pro.local>
2016-08-21 08:32:02 +0800

    Add nav styling

---
+++ app/assets/javascripts/app.js
@@ -0,0 +1,8 @@
+kissuiScrollAnim.add(document.getElementsByClassName('text')[0], {
+  'in': 'fadeIn',
+  'top': 'fadeOut'
+});
+kissuiScrollAnim.add(document.getElementsByClassName('notif')[0], {
+  'in': 'fadeIn',
+  'top': 'fadeOut'
+});
